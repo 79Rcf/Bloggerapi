3d1691a6f191e33e8863cd9dd4b9c2dc1e780f2d
Ulrich Killian <fastfrightshipping@gmail.com>
2025-09-10 10:24:56 +0100

    added a git gitignore file

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ import commentsRoute from "./routes/comments.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
